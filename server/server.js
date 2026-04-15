74aa74ff371af0a21bafa3a1bd044d26fc11a360
Harilal <harilalkumar821112@gmail.com>
2026-04-16 02:53:50 +0530

    fix: always listen on PORT for Render deployment

--- server/server.js
+++ server/server.js
@@ -75,10 +75,8 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-if (!process.env.VERCEL) {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 module.exports = app;
